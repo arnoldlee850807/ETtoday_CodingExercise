72dd9e611b157e5aa9813dfd1782f492e52b669b
Arnold Lee <arnold.kn.lee@gmail.com>
2024-07-22 10:57:03 +0800

    Initial Commit

---
+++ ETtoday_CodingExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ETtoday_CodingExercise
+//
+//  Created by Arnold on 7/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
